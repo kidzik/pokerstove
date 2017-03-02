f9e1d3ea78ff1dc4daa97bf24668c9b51fdae3c9
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2017-03-02 09:33:58 -0800

    top n%

--- src/programs/ps-omaha/main.cpp
+++ src/programs/ps-omaha/main.cpp
@@ -11,6 +11,44 @@ using namespace pokerstove;
 namespace po = boost::program_options;
 using namespace std;
 
+double calculate_equity(std::vector<Card> cards,
+			string board,
+			vector<string> hands,
+			boost::shared_ptr<PokerHandEvaluator> evaluator)
+{
+  ShowdownEnumerator showdown;
+  std::random_shuffle(cards.begin(), cards.end());
+
+  // construct oppenent's hand and the board by drawing
+  // first cards from the shuffled deck
+  CardSet newBoard = CardSet(board);
+  CardSet opponent = CardSet();
+
+  int toBoard = evaluator->boardSize() - newBoard.size();
+  for (int i = 0; i < toBoard; i++){
+    newBoard.insert(cards[i]);
+  }
+  for (int i = toBoard; i < toBoard + evaluator->handSize(); i++){
+    opponent.insert(cards[i]);
+  }
+
+  // evaluation (TODO: copied from ps-eval, could be done better!)
+    
+  // allocate evaluator and create card distributions
+  vector<CardDistribution> handDists;
+
+  handDists.emplace_back();
+  handDists.back().parse(hands[0]);
+  handDists.emplace_back();
+  handDists.back().parse(opponent.str());
+
+  // calcuate the results and store them
+  vector<EquityResult> results =
+    showdown.calculateEquity(handDists, newBoard, evaluator);
+
+  return results[0].winShares + results[0].tieShares;
+}
+
 int main(int argc, char** argv) {
   po::options_description desc("ps-eval, a poker hand evaluator\n");
 
@@ -18,6 +56,7 @@ int main(int argc, char** argv) {
       ("game,g", po::value<string>()->default_value("o"), "game to use for evaluation")
       ("board,b", po::value<string>(), "community cards for he/o/o8")
       ("hand,h", po::value<vector<string>>(), "a hand for evaluation")
+      ("top,top", po::value<int>(), "% of top hands")
       ("samples,s", po::value<int>()->default_value(10000), "num of monte carlo samples");
   // TODO: Only Omaha works!
   // TODO: Only one hand!
@@ -62,42 +101,38 @@ int main(int argc, char** argv) {
 
   int samples = vm["samples"].as<int>();
 
-  double total = 0.0;
   boost::shared_ptr<PokerHandEvaluator> evaluator =
     PokerHandEvaluator::alloc(game);
-  ShowdownEnumerator showdown;
 
+  // equity vs random hand
+  double total = 0.0;
+  for (int s = 0; s < samples; s++)
+    total += calculate_equity(cards, board, hands, evaluator);
+
+  // equity vs pre-flop top 20%
+  std::map<CardSet, double> random_hands;
   for (int s = 0; s < samples; s++){
+    CardSet fullSetTmp = CardSet();
     std::random_shuffle(cards.begin(), cards.end());
-
-    // construct oppenent's hand and the board by drawing
-    // first cards from the shuffled deck
-    CardSet newBoard = CardSet(board);
     CardSet opponent = CardSet();
 
-    int toBoard = evaluator->boardSize() - newBoard.size();
-    for (int i = 0; i < toBoard; i++){
-      newBoard.insert(cards[i]);
-    }
-    for (int i = toBoard; i < toBoard + evaluator->handSize(); i++){
+    for (int i = 0; i < 5; i++){
       opponent.insert(cards[i]);
     }
+    CardSet opponentSet = CardSet(opponent);
+    fullSetTmp ^= opponentSet;
+    std::vector<Card> cards = fullSetTmp.cards();
 
-    // evaluation (TODO: copied from ps-eval, could be done better!)
-    
-    // allocate evaluator and create card distributions
-    vector<CardDistribution> handDists;
-
-    handDists.emplace_back();
-    handDists.back().parse(hands[0]);
-    handDists.emplace_back();
-    handDists.back().parse(opponent.str());
-
-    // calcuate the results and store them
-    vector<EquityResult> results =
-      showdown.calculateEquity(handDists, newBoard, evaluator);
-
-    total += results[0].winShares + results[0].tieShares;
+    vector<string> opponent_hand;
+    opponent_hand.push_back(opponent.str());
+    cout << opponent.str() << endl;
+    double eq = calculate_equity(cards, board, opponent_hand, evaluator);
+    cout << eq << endl;
+    random_hands[opponent] = eq;
+    cout << "DONE" << endl;
   }
+  calculate_equity(cards, board, hands, evaluator);
+
+
   cout << (total / double(samples)) << endl;
 }
