630353ebde917716a936f4d4639c25c03f91ffd1
Ubuntu <ubuntu@ip-172-31-40-20.eu-west-1.compute.internal>
2017-03-06 07:03:14 +0000

    More comments

--- src/programs/ps-omaha/main.cpp
+++ src/programs/ps-omaha/main.cpp
@@ -19,6 +19,11 @@ struct more_first {
     }
 };
 
+// cards - cards not dealt
+// board - board
+// hands - vector of hands
+// evaluator - object evaluating the showdown
+// samples - number of samples
 pair<double, double> calculate_equity(std::vector<Card> cards,
 			string board,
 			vector<string> hands,
@@ -26,43 +31,49 @@ pair<double, double> calculate_equity(std::vector<Card> cards,
 			int samples)
 {
   pair<double, double> total(0.0,0.0);
-  for (int s = 0; s < samples; s++){
-  ShowdownEnumerator showdown;
-  std::random_shuffle(cards.begin(), cards.end());
 
-  // construct oppenent's hand and the board by drawing
-  // first cards from the shuffled deck
-  CardSet newBoard = CardSet(board);
-  CardSet opponent = CardSet();
+  for (int s = 0; s < samples; s++)
+  {
+    ShowdownEnumerator showdown;
+    std::random_shuffle(cards.begin(), cards.end());
 
-  int toBoard = evaluator->boardSize() - newBoard.size();
-  for (int i = 0; i < toBoard; i++){
-    newBoard.insert(cards[i]);
+    // construct oppenent's hand and the board by drawing
+    // first cards from the shuffled deck
+    CardSet newBoard = CardSet(board);
+    CardSet opponent = CardSet();
+
+    int toBoard = evaluator->boardSize() - newBoard.size();
+    for (int i = 0; i < toBoard; i++){
+      newBoard.insert(cards[i]);
+    }
+    for (int i = toBoard; i < toBoard + evaluator->handSize(); i++){
+      opponent.insert(cards[i]);
+    }
+    // by now everything is dealt
+
+    // prepare an object for evaluation
+    vector<CardDistribution> handDists;
+
+    // add hands
+    handDists.emplace_back();
+    handDists.back().parse(hands[0]); // hero
+    handDists.emplace_back();
+
+    // if there are two hands we add them
+    // if not, we add the generated opponent
+    if (hands.size() > 1)
+      handDists.back().parse(hands[1]);
+    else
+      handDists.back().parse(opponent.str());
+
+    // calcuate the results and store them
+    vector<EquityResult> results =
+      showdown.calculateEquity(handDists, newBoard, evaluator);
+
+    total.first += results[0].winShares + results[0].tieShares;
+    total.second += results[1].winShares + results[1].tieShares;
   }
-  for (int i = toBoard; i < toBoard + evaluator->handSize(); i++){
-    opponent.insert(cards[i]);
-  }
-
-  // evaluation (TODO: copied from ps-eval, could be done better!)
-    
-  // allocate evaluator and create card distributions
-  vector<CardDistribution> handDists;
-
-  handDists.emplace_back();
-  handDists.back().parse(hands[0]);
-  handDists.emplace_back();
-  if (hands.size() > 1)
-    handDists.back().parse(hands[1]);
-  else
-    handDists.back().parse(opponent.str());
-
-  // calcuate the results and store them
-  vector<EquityResult> results =
-    showdown.calculateEquity(handDists, newBoard, evaluator);
-
-  total.first += results[0].winShares + results[0].tieShares;
-  total.second += results[1].winShares + results[1].tieShares;
-  }
+  // mean
   total.first /= samples;
   total.second /= samples;
   return total;
@@ -103,8 +114,9 @@ int main(int argc, char** argv) {
   // extract the options
   string game = vm["game"].as<string>();
   int top = vm["top"].as<int>();
-  string board = vm.count("board") ? vm["board"].as<string>() : "";
+  string board = (vm.count("board") ) ? vm["board"].as<string>() : "";
   vector<string> hands = vm["hand"].as<vector<string>>();
+  int samples = vm["samples"].as<int>();
 
   // build a deck for simulations
   CardSet fullSet = CardSet();
@@ -119,8 +131,6 @@ int main(int argc, char** argv) {
   // list the cards
   std::vector<Card> cards = fullSet.cards();
 
-  int samples = vm["samples"].as<int>();
-
   boost::shared_ptr<PokerHandEvaluator> evaluator =
     PokerHandEvaluator::alloc(game);
 
@@ -128,31 +138,34 @@ int main(int argc, char** argv) {
   if (top == 100){
     pair<double, double> total = calculate_equity(cards, board, hands, evaluator, samples);
     cout << total.first << endl;
+    return 0;
   }
 
   // equity vs pre-flop top 20%
   std::map<CardSet, double> random_hands;
 
-  // get pre-flop hands
+  // get hands
+  // CardSet fullSetTmp = CardSet();
+  // fullSetTmp.fill();
+  // fullSetTmp ^= handSet;
+  // fullSetTmp ^= boardSet;
+  // std::vector<Card> cards = fullSetTmp.cards();
+
   for (int s = 0; s < samples; s++){
-    CardSet fullSetTmp = CardSet();
-    fullSetTmp.fill();
-    fullSetTmp ^= handSet;
-    std::vector<Card> cards = fullSetTmp.cards();
     std::random_shuffle(cards.begin(), cards.end());
     CardSet opponent = CardSet();
 
     for (int i = 0; i < 4; i++){
        opponent.insert(cards[i]);
     }
-    CardSet opponentSet = CardSet(opponent);
-    fullSetTmp ^= opponentSet;
-    cards = fullSetTmp.cards();
+    fullSet ^= opponent;
+    std::vector<Card> cards = fullSet.cards();
 
     vector<string> opponent_hand;
     opponent_hand.push_back(opponent.str());
-    pair<double, double> eq = calculate_equity(cards, "", opponent_hand, evaluator, 100);
+    pair<double, double> eq = calculate_equity(cards, board, opponent_hand, evaluator, 100);
     random_hands[opponent] = eq.first;
+    fullSet |= opponent;
   }
   
   // sort hands
@@ -165,16 +178,17 @@ int main(int argc, char** argv) {
 
   // equity vs top 10%
 
-  int nruns = samples / 10;
-  double eq_vs_top_flop = 0.0;
+  int nruns = samples * top / 100;
+  double eq_vs_top = 0.0;
   for (int s = 0; s < nruns; s++){
     vector<string> vhands;
     vhands.push_back(hands[0]);
     vhands.push_back(eq_random_hands[s].second.str());
-    cout << eq_random_hands[s].second.str() << endl;
-    pair<double, double> eq = calculate_equity(cards, board, vhands, evaluator, 100);
-    eq_vs_top_flop += eq.first;
+    // cout << eq_random_hands[s].second.str() << endl;
+    pair<double, double> eq = calculate_equity(cards, board, vhands, evaluator, 1);
+    eq_vs_top += eq.first;
   }
-  eq_vs_top_flop /= nruns;
-  cout << eq_vs_top_flop << endl;
+  eq_vs_top /= nruns;
+  cout << eq_vs_top << endl;
+  return 0;
 }
